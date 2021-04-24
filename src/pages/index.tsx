10bd239d2a0e74be14cc16d26915719b6e313e9b
Stiven <stivenfortes96@gmail.com>
2021-04-24 15:22:02 -0100

    Configured Json-server and Fetched Data from api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import { Header } from "../components/Header";
+//SPA
+//SSR
+//SSG
 
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) {
+
+  /* useEffect(()=>{ SPA Rendering
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, []) */
   return (
-   <h1>Index</h1>
+    <div>
+
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+} 
